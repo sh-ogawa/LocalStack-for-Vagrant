54f6de84875267f751d2f2da93a4746502d2bb01
sh-ogawa <syogo0417@gmail.com>
2018-05-10 13:13:18 +0900

    s3 event throw sqs

--- provision/roles/s3-sqs/files/s3-sqs-sample.sh
+++ provision/roles/s3-sqs/files/s3-sqs-sample.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 aws --endpoint-url=http://localhost:4572 s3api put-bucket-notification-configuration \
-    --bucket local-test-bucket --notification-configuration file://notification.json
+    --bucket local-test-bucket --notification-configuration '{
+  "QueueConfigurations": [
+    {
+      "Id":"test-event",
+      "QueueArn": "arn:aws:sqs:elasticmq:000000000000:test-queue",
+      "Events": ["s3:ObjectCreated:Post"],
+      "Filter": {
+        "Key": {
+          "FilterRules": [{ "Name": "suffix","Value": ".jpg" }]
+        }
+      }
+    }
+  ]
+}'
